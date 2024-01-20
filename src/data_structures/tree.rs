1e90e6506d7c7bad99d7dce834461ce59870873c
GameHunter101 <liorscarmeli@gmail.com>
2024-01-19 20:56:21 -0500

    Added error handling to indexing, some more features

--- src/data_structures/tree.rs
+++ src/data_structures/tree.rs
@@ -1,4 +1,12 @@
-use std::ops::{Index, IndexMut};
+use std::{
+    fmt::{Debug, write},
+    ops::{Index, IndexMut},
+};
+
+#[derive(Debug)]
+pub enum TreeError {
+    IndexOutOfBoundsError,
+}
 
 #[derive(Debug)]
 pub struct Tree<T> {
@@ -6,7 +14,7 @@ pub struct Tree<T> {
     children: Vec<Tree<T>>,
 }
 
-impl<T> Tree<T> {
+impl<T: Debug> Tree<T> {
     pub fn new(root: T) -> Self {
         Self {
             node: root,
@@ -27,7 +35,47 @@ impl<T> Tree<T> {
     }
 
     pub fn max_depth(&self) -> usize {
-        todo!("Do some depth-first-search to find the total depth of the current root tree")
+        if self.is_leaf() {
+            return 1;
+        }
+        let mut children_length = self
+            .children
+            .iter()
+            .map(|child| child.max_depth())
+            .collect::<Vec<_>>();
+        children_length.sort();
+        return 1 + children_length.last().unwrap();
+    }
+
+    pub fn is_leaf(&self) -> bool {
+        self.children.len() == 0
+    }
+
+    pub fn index_depth(&self, index: Vec<usize>) -> Result<&Self, TreeError> {
+        let mut current_node = self;
+        for i in index {
+            if current_node.children.len() == 0 {
+                return Err(TreeError::IndexOutOfBoundsError);
+            }
+            current_node = &current_node[i];
+        }
+        Ok(current_node)
+    }
+
+    pub fn index_mut_depth(&mut self, index: Vec<usize>) -> Result<&mut Self, TreeError> {
+        let mut current_node = self;
+        for i in index {
+            if current_node.children.len() == 0 {
+                return Err(TreeError::IndexOutOfBoundsError);
+            }
+            current_node = &mut current_node[i];
+        }
+        Ok(current_node)
+    }
+
+    pub fn append_at_depth(&mut self, index: Vec<usize>, node: T) -> Result<(), TreeError> {
+        self.index_mut_depth(index)?.children.push(Tree::new(node));
+        Ok(())
     }
 }
 
@@ -44,27 +92,6 @@ impl<T> IndexMut<usize> for Tree<T> {
     }
 }
 
-impl<T> Index<Vec<usize>> for Tree<T> {
-    type Output = Tree<T>;
-    fn index(&self, index: Vec<usize>) -> &Self::Output {
-        let mut current_node = self;
-        for i in index {
-            current_node = &current_node[i];
-        }
-        current_node
-    }
-}
-
-impl<T> IndexMut<Vec<usize>> for Tree<T> {
-    fn index_mut(&mut self, index: Vec<usize>) -> &mut Self::Output {
-        let mut current_node = self;
-        for i in index {
-            current_node = &mut current_node[i];
-        }
-        current_node
-    }
-}
-
 #[test]
 fn tree_test() {
     let mut tree = Tree::new(0);
@@ -72,8 +99,11 @@ fn tree_test() {
     for i in 0..3 {
         tree.add_child_node(i);
         for j in 0..2 {
-            tree[i].add_child_node(j);
+            tree[i].add_child_node(8);
         }
     }
-    dbg!(tree);
+    tree.append_at_depth(vec![0], 324);
+    dbg!(&tree);
+    let val = tree.index_depth(vec![0,2]).unwrap().node();
+    assert_eq!(324, *val);
 }
